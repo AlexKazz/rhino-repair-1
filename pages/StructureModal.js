5ed8c4ee9d3fe9c42bb8dcc317a0ef4b55aca422
AlexKazz <akazenoff@gmail.com>
2023-05-20 17:36:45 -0700

    improve modal css

--- pages/StructureModal.js
+++ pages/StructureModal.js
@@ -4,6 +4,14 @@ import styles from "./Home.module.css";
 
 Modal.setAppElement("#__next");
 
+const customStyles = {
+  content: {
+    width: "400px",
+    height: "600px",
+    margin: "auto",
+  },
+};
+
 function StructureModal({
   modalIsOpen,
   onRequestClose,
@@ -14,39 +22,78 @@ function StructureModal({
   handleDelete,
 }) {
   return (
-    <Modal isOpen={modalIsOpen} onRequestClose={onRequestClose}>
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+    >
       <h2>Structure Details</h2>
+      <div className={styles.inputField}>
+        <label>Title:</label>
+        <input
+          name="title"
+          placeholder="Title"
+          value={currentStructure.title}
+          onChange={handleInputChange}
+        />
+      </div>
+      <br />
+      <label>Upload Structure Photo:</label>
       <input
-        name="title"
-        placeholder="Title"
-        value={currentStructure.title}
-        onChange={handleInputChange}
+        className="mb-6"
+        type="file"
+        name="image"
+        onChange={handleFileChange}
       />
+
       <br />
-      <input type="file" name="image" onChange={handleFileChange} />
+      <div className={styles.inputField}>
+        <label>X Coordinate:</label>
+        <input
+          name="xCoordinate"
+          placeholder="Rhino X Coordinate"
+          value={currentStructure.xCoordinate}
+          onChange={handleInputChange}
+        />
+      </div>
       <br />
+      <div className={styles.inputField}>
+        <label>Y Coordinate:</label>
+        <input
+          name="yCoordinate"
+          placeholder="Rhino Y Coordinate"
+          value={currentStructure.yCoordinate}
+          onChange={handleInputChange}
+        />
+      </div>
+      <br />
+      <div className={styles.inputField}>
+        <label>Z Coordinate:</label>
+        <input
+          name="zCoordinate"
+          placeholder="Rhino Z Coordinate"
+          value={currentStructure.zCoordinate}
+          onChange={handleInputChange}
+        />
+      </div>
+      <br />
+      <label>Upload Rhino3D File:</label>
       <input
-        name="xCoordinate"
-        placeholder="X Coordinate"
-        value={currentStructure.xCoordinate}
-        onChange={handleInputChange}
+        className="mb-6"
+        type="file"
+        name="rhinoFile"
+        onChange={handleFileChange}
       />
       <br />
-      <input
-        name="yCoordinate"
-        placeholder="Y Coordinate"
-        value={currentStructure.yCoordinate}
-        onChange={handleInputChange}
-      />
-      <br />
-      <input type="file" name="rhinoFile" onChange={handleFileChange} />
-      <br />
-      <textarea
-        name="notes"
-        placeholder="Notes"
-        value={currentStructure.notes}
-        onChange={handleInputChange}
-      />
+      <div className={styles.inputField}>
+        <label>Notes:</label>
+        <textarea
+          name="notes"
+          placeholder="Notes"
+          value={currentStructure.notes}
+          onChange={handleInputChange}
+        />
+      </div>
       <br />
       <button onClick={handleSave} className={styles.button}>
         Save
